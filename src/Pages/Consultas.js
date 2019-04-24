fb12cb93c87d652614890752d8ead597c3fe18f5
Davi Tavares Da Silva <44851410808@info.local>
2019-04-24 10:55:39 -0300

    algumaCoisa

---
+++ src/Pages/Consultas.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class Consultas extends Component {
+
+    render() {
+        return ('batatinha');
+    }
+
+
+}
+export default Consultas;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,63 +1,76 @@
 
 import React, { Component } from "react";
-import {parseJwt} from '../../services/auth';
-import {Link} from 'react-router-dom';
+import { parseJwt } from '../Services/Auth';
 
-class longin extends Component{
+import Axios from 'axios';
 
-    render(){
-        return (
-            <section className="container flex">
-              <div className="img__login">
-                <div className="img__overlay" />
-              </div>
-      
-              <div className="item__login">
-                <div className="row">
-                  <div className="item">
-                    <Link to="/">
-                      <img src={logo} className="icone__login" alt="SviGufo" />
-                    </Link>
-                  </div>
-                  <div className="item" id="item__title">
-                    <p className="text__login" id="item__description">
-                      Bem-vindo! Faça login para acessar sua conta.
-                    </p>
-                  </div>
-                  <form onSubmit={this.efetuaLogin.bind(this)}>
-                    <div className="item">
-                      <input
-                        className="input__login"
-                        placeholder="username"
-                        type="text"
-                        value={this.state.email}
-                        onChange={this.atualizaEstadoEmail.bind(this)}
-                        name="username"
-                        id="login__email"
-                      />
-                    </div>
-                    <div className="item">
-                      <input
-                        className="input__login"
-                        placeholder="password"
-                        value={this.state.senha}
-                        onChange={this.atualizaEstadoSenha.bind(this)}
-                        type="password"
-                        name="password"
-                        id="login__password"
-                      />
-                    </div>
-                    <p className="text__login" style={{ color : 'red',  textAlign : 'center' }}>{this.state.erroMensagem}</p>
-                    <div className="item">
-                      <button type="submit" className="btn btn__login" id="btn__login">
-                        Login
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </section>
-          );
+class longin extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      erroMensagem: ''
     }
+  }
+
+  atualizaEstadoEmail(event) {
     
+    this.setState({ email: event.target.value });
+  }
+
+  atualizaEstadoSenha(event) {
+    this.setState({ senha: event.target.value });
+  }
+
+
+  bondedoLogin(event) {
+    event.preventDefault();
+
+    Axios.post("http://localhost:64582/api/Login",
+      {
+        email: this.state.email,
+        senha: this.state.senha
+      })
+      .then(data => {
+        if (data.status === 200) {
+          console.log(data);
+          localStorage.setItem("usuario-logado", data.data.token);
+
+          this.props.history.push("/consultas");
+        }
+      })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.bondedoLogin.bind(this)}>
+          <input type="text"
+            className="email"
+            placeholder="Usuário"
+            value={this.state.email}
+            onChange={this.atualizaEstadoEmail.bind(this)}
+          ></input>
+          <input
+            type="password"
+            className="password"
+            placeholder="Senha"
+            value={this.state.senha}
+            onChange={this.atualizaEstadoSenha.bind(this)}
+          >
+          </input>
+          <button type="submit" className="login" id="btn_login">
+            Login
+        </button>
+        </form>
+      </div>
+
+    );
+  }
+
 }
+export default longin;
